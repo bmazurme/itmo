90a0769b975987907b794bcb1de7964e4d555de7
BMAZUR <49879328+bmazurme@users.noreply.github.com>
2022-03-24 17:03:48 +0300

    update

--- src/components/Card.js
+++ src/components/Card.js
@@ -9,10 +9,10 @@ function Card(props) {
     <div className='card'>
       <div onPointerEnter={handleClick}
            onPointerLeave={handleClick} 
-           className={`card__frame ${active && 'card__frame_active'}`}>
+           className={`card__frame ${props.type && (`card__frame_${props.type}`)} ${active && 'card__frame_active'}`}>
 
         <div className='card__title'>Анализ социальных данных</div>
-        <div className={`card__icon ${props.type}`}></div>
+        <div className={`card__icon card__icon_${props.type}`}></div>
         <div className='card__content'>Направление включает в себя исследования и разработки технологий МО и КТ</div>
         <div className={`card__button ${active && 'card__button_active'}`}>Подробнее</div>
       </div>
