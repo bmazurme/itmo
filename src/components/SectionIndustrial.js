918cf27268c31fdf807c9780c1cf488bcad86b5e
Bogdan <b@bmazur.ru>
2022-03-27 15:36:08 +0300

    update

--- src/components/SectionIndustrial.js
+++ src/components/SectionIndustrial.js
@@ -7,6 +7,28 @@ function SectionIndustrial() {
       <h2 className="section-industrial__title">
         INDUSTRIAL AI RESEARCH LAB
       </h2>
+
+      <div className="section-industrial__container">
+
+        <p className="section-industrial__paragraph">
+          Центр национальной технологической инициативы (НТИ), 
+          получивший государственную поддержку 
+          по направлению «Технологии машинного обучения 
+          и когнитивные технологии». 
+        </p>
+        
+        <p className="section-industrial__paragraph">
+          Мы изучаем проблемы и выявляем, когда мы работаем 
+          вместе с нашими клиентами, новые решения 
+          находят, не задумываясь. Работая таким образом 
+          и проявляя гибкость, мы можем поддерживать новый 
+          подход для удовлетворения потребностей наших клиентов.
+        </p>
+
+        <img className="section-industrial__image" src='' alt=''/>
+        <img className="section-industrial__image" src='' alt=''/>
+      </div>
+
       <div className='section-industrial__cards'>
         {cards.map((card, index) => 
           <Card key={index} 
