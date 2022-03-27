a75db93214bfa5288e9537b9155f083f9ef042ac
Bogdan <b@bmazur.ru>
2022-03-27 15:10:00 +0300

    update

--- src/components/SectionForm.js
+++ src/components/SectionForm.js
@@ -1,7 +1,14 @@
 function SectionForm() {
   return(
     <section className="section-form">
-      <h2 className="section-form__title">ВСЕГДА НА СВЯЗИ</h2>
+      <h2 className="section-form__title">
+        ВСЕГДА НА СВЯЗИ
+      </h2>
+      <p className="section-form__content">
+        По всем интересующим вас вопросам вы всегда можете обратиться 
+        по указанным телефонам, написать на почту или оставтиь свое 
+        сообщение через форму обратной связи. 
+      </p>
     </section>
   );
 }
