5b45c15a5a10dabc470df579df55ddcf5cd8cd6f
Bogdan <b@bmazur.ru>
2022-04-01 18:55:02 +0300

    feat: popup

---
+++ src/components/Popup.js
@@ -0,0 +1,15 @@
+function Popup() {
+  return(
+    <div className="popup">
+      <div className="popup__container">
+        <div className="popup__button"></div>
+        <img className="popup__image" src="" alt=""></img>
+        <h2 className="popup__title">Анализ социальных данных</h2>
+        <p className="popup__content">
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Popup;
